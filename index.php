d4065d290291f96f844f313450dc3d279c780b9b
michaeljrs <38431821+michaeljrs@users.noreply.github.com>
2018-09-28 11:09:26 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,10 +4,15 @@ require 'vendor/autoload.php';
 
 function ServiceHandler() {
     $data = new stdClass();
-    $data->MeuDado = "Hello World!";
-
+    $data->Nome = "Michael";
+    $data->CPF = "071.100.407-20";
+        $data2 = new stdClass();
+        $data2->Logradouro = "Rua Jornalista Luis Eduardo Lobo";
+        $data2->Numero = "437";
+    $data->Endereco = $data2;
     return json_encode($data);
 };
+
 
 $app = new \Slim\App;
 
