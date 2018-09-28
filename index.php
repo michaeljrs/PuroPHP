a4abee5429b22e11e50afdb6b42b245653d57e88
michaeljrs <38431821+michaeljrs@users.noreply.github.com>
2018-09-28 12:28:38 -0300

    1

--- index.php
+++ index.php
@@ -2,9 +2,61 @@
 
 require 'vendor/autoload.php';
 
+
+
+function ExecuteMySQL($query)
+{
+
+
+	$servername = "sdi.iabas.org.br:3306";
+	$username = "root";
+	$password = "010572!dev";
+	//$DB="portalinfo";
+	$DB="indicadores";
+
+	// Create connection
+	$conn = new mysqli($servername, $username, $password,$DB);
+
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	} ;
+
+	$conn->set_charset("utf8");
+
+	$result = $conn->query($query);
+
+	if ($conn->error!='') {
+	
+		echo "Ocorrencia [ " . $conn->error."]<BR>" ;
+	}
+
+	//echo ("rodou".$result);
+	return $result;
+}
+
+function rectoARRAY($rec)
+{
+	$array_=[];
+	$count=0;
+	while($row = $rec->fetch_assoc()) {
+		$count++;
+		$array_[$count] = $row;
+	}
+
+	return $array_;
+
+}
+
+
 function ServiceHandler() {
 
 
+    $query="select  VARIAVEL_ID,VARIAVEL_NOME  from VARIAVEL order by VARIAVEL_ID ";
+    $result=ExecuteMySQL($query);
+    $recV=rectoARRAY($result);
+
+    $j=json_encode(array_values($recV));
 
     $data = new stdClass();
     $data->Nome = "Michael";
@@ -15,8 +67,8 @@ function ServiceHandler() {
         $data2->Complemento = "437";
     $data->Endereco = $data2;
 
-    
-    return json_encode($data);
+    return $j;
+   # return json_encode($data);
 };
 
 
